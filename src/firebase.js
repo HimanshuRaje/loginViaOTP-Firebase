db525b993d04680f36deec7b8d7dc8e0b1898cae
Himanshu Jagtap <163515380+HimanshuRaje@users.noreply.github.com>
2025-07-23 18:46:04 +0530

    essential changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,14 @@
-// src/firebase.js
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "###################################",
-  authDomain: "#########################",
-  projectId: "####################",
-  storageBucket: "#######################",
-  messagingSenderId: "##################",
-  appId: "############################"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-export { auth, firebase };
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
