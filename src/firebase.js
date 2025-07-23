2e617718490a5572c6956e173c0f4678b0fefbe5
Himanshu Jagtap <163515380+HimanshuRaje@users.noreply.github.com>
2025-07-23 18:41:50 +0530

    made some essential changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ import 'firebase/compat/auth';
 
 const firebaseConfig = {
   apiKey: "###################################",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  authDomain: "#########################",
+  projectId: "####################",
+  storageBucket: "#######################",
+  messagingSenderId: "##################",
+  appId: "############################"
 };
 
 // Initialize
